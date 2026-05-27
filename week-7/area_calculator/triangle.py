e291dd787ef4baa4703b8df991ff7686190fe66c
YE <yaakovde123@gmail.com>
2026-05-27 15:51:26 +0300

    completed the triangle file

---
+++ week-7/area_calculator/triangle.py
@@ -0,0 +1,15 @@
+from rectangle import Rectangle
+
+class Triangle(Rectangle):
+    def __init__(self,width, height, base, side_a, side_b, side_c):
+        super().__init__(width, height)
+        self.base = base
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+
+    def get_area(self):
+        return (self.base * self.height) / 2
+    
+    def get_perimeter(self):
+        return self.side_a + self.side_b + self.side_c
