1292caafc34f6d327699bac5de28654ad629a50e
YE <yaakovde123@gmail.com>
2026-05-27 15:52:53 +0300

    completed the main file

---
+++ week-7/area_calculator/main.py
@@ -0,0 +1,14 @@
+import calculater, hexagon, rectangle, square, triangle, circle
+
+hex = hexagon.Hexagon(5)
+sqre = square.Square(2,3,4)
+rect = rectangle.Rectangle(2,3)
+trngl = triangle.Triangle(1,2,3,4,5,6)
+crlce = circle.Circle(1)
+shape = calculater.Shape()
+print(hex)
+print(sqre)
+print(rect)
+print(trngl)
+print(crlce)
+print(repr(rectangle))
