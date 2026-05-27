916e3e3ad498f84bc2278e056b4fc92ec4d637e1
YE <yaakovde123@gmail.com>
2026-05-27 15:49:25 +0300

    completed calculator file

---
+++ week-7/area_calculator/calculater.py
@@ -0,0 +1,21 @@
+
+class Shape():
+  
+    def get_area():
+        ...
+    
+    def get_perimeter():
+        ...
+
+    def __str__(self):
+        return f" Shape: {type(self).__name__}\n Area: {self.get_area()}\n Perimeter: {self.get_perimeter()}"
+    
+    def __repr__(self):
+        return f"Type:{type(self)}"
+
+def __setattr__(self, name, value):
+    if not value > 0:
+        raise ValueError("Value must be positive")
+    if not isinstance(value,int|float): 
+        raise ValueError("values must be numbers")
+    super().__setattr__(name,value)
